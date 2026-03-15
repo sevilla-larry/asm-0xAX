95e000272c998d595e8e2130bcea1298a9c83bb6
Sevilla, Larry <sevilla.larry.prg@gmail.com>
2026-03-15 15:55:05 +0800

    upload casm's

---
+++ Part1/casm.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+        char* str = "Hello World\n";
+        long len = strlen(str);
+        int ret = 0;
+
+
+        __asm__("movq $1, %%rax \n\t"     // rax = 1 - Specify the number of the system call (1 is `sys_write`).
+                "movq $1, %%rdi \n\t"     // rdi = 1 - Set the first argument of `sys_write` to 1 (`stdout`).
+                "movq %1, %%rsi \n\t"     // rsi = str - Set the second argument of `sys_write` to the reference of the `str` variable.
+                "movq %2, %%rdx \n\t"     // rdx = len(str) - Set the third argument of `sys_write` to the length of the `str` variable's value.
+                "syscall"                 // Call the `sys_write` system call.
+                : "=r"(ret)               // Return the result in the `ret` variable.
+                : "g"(str), "g" (len)     // Put `str` and `len` variables in any general operand (memory, register, or immediate, if possible)
+//                : "rax", "rdi", "rsi", "rdx", "rcx", "r11", "memory"
+        );
+
+/*
+        __asm__ volatile (
+                ".intel_syntax noprefix;" // Switch to Intel/NASM style
+                "mov rax, 1;"             // sys_write
+                "mov rdi, 1;"             // fd 1 (stdout)
+                "mov rsi, %1;"            // buffer address
+                "mov rdx, %2;"            // count
+                "syscall;"                // invoke kernel
+                "mov %0, rax;"            // move return value to 'ret'
+                ".att_syntax;"            // Switch back to AT&T for the compiler
+                : "=r" (ret)              // %0: Output (using a register)
+                : "r" (str), "r" (len)    // %1, %2: Inputs
+                : "rax", "rdi", "rsi", "rdx", "rcx", "r11" // Clobbered registers
+            );
+
+            asm volatile (
+                ".intel_syntax noprefix     \n\t"
+                "mov rax, 1                 \n\t"
+                "mov rdi, 1                 \n\t"
+                "mov rsi, %1                \n\t"
+                "mov rdx, %2                \n\t"
+                "syscall                    \n\t"
+                ".att_syntax prefix"          // important: switch back!
+                : "=r" (ret)
+                : "r" (str), "r" (len)
+                : "rax", "rdi", "rsi", "rdx", "rcx", "r11"
+            );
+*/
+
+printf("Bytes written: %d\n", ret);
+        return 0;
+}
---
+++ Part1/casm1.asm
@@ -0,0 +1,31 @@
+;; Definition of the `data` section
+section .data
+        ;; String variable with the value `hello world!`
+        ;; `10` is the ASCII code of the new line symbol.
+        msg db "hello, world!", 10
+        MSG_LEN equ $ - msg
+
+        ;; Reference to the C stdlib functions that we will use
+        extern write, exit
+
+;; Definition of the text section
+section .text
+        ;; Reference to the entry point of our program
+        global _start
+
+;; Entry point
+_start:
+        ;; Set the first argument of the `write` function to 1 (`stdout`).
+        mov rdi, 1
+        ;; Set the second argument of the `write` function to the reference of the `msg` variable.
+        mov rsi, msg
+        ;; Set the third argument to the length of the `msg` variable's value (14 bytes).
+;        mov rdx, 14
+        mov rdx, MSG_LEN
+        ;; Call the `write` function.
+        call write
+
+        ;; Set the first argument of `sys_exit` to 0. The 0 status code is success.
+        mov rdi, 0
+        ;; Call the `exit` function
+        call exit
