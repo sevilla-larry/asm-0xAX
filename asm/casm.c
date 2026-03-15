28076054904127aab5ed51e964e9997cb409165b
Sevilla, Larry <sevilla.larry.prg@gmail.com>
2026-03-15 16:10:23 +0800

    change directory

--- asm/casm.c
+++ asm/casm.c
@@ -6,18 +6,19 @@ int main() {
         long len = strlen(str);
         int ret = 0;
 
-
         __asm__("movq $1, %%rax \n\t"     // rax = 1 - Specify the number of the system call (1 is `sys_write`).
                 "movq $1, %%rdi \n\t"     // rdi = 1 - Set the first argument of `sys_write` to 1 (`stdout`).
                 "movq %1, %%rsi \n\t"     // rsi = str - Set the second argument of `sys_write` to the reference of the `str` variable.
                 "movq %2, %%rdx \n\t"     // rdx = len(str) - Set the third argument of `sys_write` to the length of the `str` variable's value.
                 "syscall"                 // Call the `sys_write` system call.
-                : "=r"(ret)               // Return the result in the `ret` variable.
-                : "g"(str), "g" (len)     // Put `str` and `len` variables in any general operand (memory, register, or immediate, if possible)
-//                : "rax", "rdi", "rsi", "rdx", "rcx", "r11", "memory"
+//              : "=r" (ret)              // Return the result in the `ret` variable.     good
+                : "=g" (ret)              // Return the result in the `ret` variable.     safe
+                : "g" (str), "g" (len)    // Put `str` and `len` variables in any general operand (memory, register, or immediate, if possible)
+//              : "rax", "rdi", "rsi", "rdx", "rcx", "r11", "memory"                    giving errors
         );
 
 /*
+        error %rbp
         __asm__ volatile (
                 ".intel_syntax noprefix;" // Switch to Intel/NASM style
                 "mov rax, 1;"             // sys_write
@@ -28,10 +29,11 @@ int main() {
                 "mov %0, rax;"            // move return value to 'ret'
                 ".att_syntax;"            // Switch back to AT&T for the compiler
                 : "=r" (ret)              // %0: Output (using a register)
-                : "r" (str), "r" (len)    // %1, %2: Inputs
-                : "rax", "rdi", "rsi", "rdx", "rcx", "r11" // Clobbered registers
+                : "g" (str), "g" (len)    // %1, %2: Inputs
+//                : "rax", "rdi", "rsi", "rdx", "rcx", "r11" // Clobbered registers
             );
 
+        error %rbp
             asm volatile (
                 ".intel_syntax noprefix     \n\t"
                 "mov rax, 1                 \n\t"
@@ -41,8 +43,8 @@ int main() {
                 "syscall                    \n\t"
                 ".att_syntax prefix"          // important: switch back!
                 : "=r" (ret)
-                : "r" (str), "r" (len)
-                : "rax", "rdi", "rsi", "rdx", "rcx", "r11"
+                : "g" (str), "g" (len)
+//                : "rax", "rdi", "rsi", "rdx", "rcx", "r11"
             );
 */
 
